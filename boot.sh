269894d49a1dfe41614db3b1c5e8a1f36827b462
Aldo W. Chacon Contreras <aldochaconc@gmail.com>
2025-11-02 22:41:24 -0300

    update branding

--- boot.sh
+++ boot.sh
@@ -3,16 +3,22 @@
 # Set install mode to online since boot.sh is used for curl installations
 export OMARCHY_ONLINE_INSTALL=true
 
-ansi_art='                 ▄▄▄                                                   
- ▄█████▄    ▄███████████▄    ▄███████   ▄███████   ▄███████   ▄█   █▄    ▄█   █▄ 
-███   ███  ███   ███   ███  ███   ███  ███   ███  ███   ███  ███   ███  ███   ███
-███   ███  ███   ███   ███  ███   ███  ███   ███  ███   █▀   ███   ███  ███   ███
-███   ███  ███   ███   ███ ▄███▄▄▄███ ▄███▄▄▄██▀  ███       ▄███▄▄▄███▄ ███▄▄▄███
-███   ███  ███   ███   ███ ▀███▀▀▀███ ▀███▀▀▀▀    ███      ▀▀███▀▀▀███  ▀▀▀▀▀▀███
-███   ███  ███   ███   ███  ███   ███ ██████████  ███   █▄   ███   ███  ▄██   ███
-███   ███  ███   ███   ███  ███   ███  ███   ███  ███   ███  ███   ███  ███   ███
- ▀█████▀    ▀█   ███   █▀   ███   █▀   ███   ███  ███████▀   ███   █▀    ▀█████▀ 
-                                       ███   █▀                                  '
+ansi_art='
+ .S_SSSs     .S_sSSs      sSSs   .S    S.     sSSs   .S_sSSs      sSSs   .S_SsS_S.    .S S.
+.SS~SSSSS   .SS~YS%%b    d%%SP  .SS    SS.   d%%SP  .SS~YS%%b    d%%SP  .SS~S*S~SS.  .SS SS.
+S%S   SSSS  S%S    S%b  d%S     S%S    S%S  d%S     S%S    S%b  d%S     S%S  Y  S%S  S%S S%S
+S%S    S%S  S%S    S%S  S%S     S%S    S%S  S%S     S%S    S%S  S%S     S%S     S%S  S%S S%S
+S%S SSSS%S  S%S    d*S  S&S     S%S SSSS%S  S&S     S%S    S&S  S&S     S%S     S%S  S%S S%S
+S&S  SSS%S  S&S   .S*S  S&S     S&S  SSS&S  S&S_Ss  S&S    S&S  S&S_Ss  S&S     S&S   SS SS
+S&S    S&S  S&S_sdSSS   S&S     S&S    S&S  S&S~SP  S&S    S&S  S&S~SP  S&S     S&S    S S
+S&S    S&S  S&S~YSY%b   S&S     S&S    S&S  S&S     S&S    S&S  S&S     S&S     S&S    SSS
+S*S    S&S  S*S    S%b  S*b     S*S    S*S  S*b     S*S    S*S  S*b     S*S     S*S    S*S
+S*S    S*S  S*S    S%S  S*S.    S*S    S*S  S*S.    S*S    S*S  S*S.    S*S     S*S    S*S
+S*S    S*S  S*S    S&S   SSSbs  S*S    S*S   SSSbs  S*S    S*S   SSSbs  S*S     S*S    S*S
+SSS    S*S  S*S    SSS    YSSP  SSS    S*S    YSSP  S*S    SSS    YSSP  SSS     S*S    S*S
+       SP   SP                         SP           SP                          SP     SP
+       Y    Y                          Y            Y                           Y      Y
+                                                                                              '
 
 clear
 echo -e "\n$ansi_art\n"
@@ -20,14 +26,14 @@ echo -e "\n$ansi_art\n"
 sudo pacman -Syu --noconfirm --needed git
 
 # Use custom repo if specified, otherwise default to basecamp/omarchy
-OMARCHY_REPO="${OMARCHY_REPO:-basecamp/omarchy}"
+OMARCHY_REPO="${OMARCHY_REPO:-aldochaconc/archenemy}"
 
 echo -e "\nCloning Omarchy from: https://github.com/${OMARCHY_REPO}.git"
 rm -rf ~/.local/share/omarchy/
 git clone "https://github.com/${OMARCHY_REPO}.git" ~/.local/share/omarchy >/dev/null
 
 # Use custom branch if instructed, otherwise default to master
-OMARCHY_REF="${OMARCHY_REF:-master}"
+OMARCHY_REF="${OMARCHY_REF:-main}"
 if [[ $OMARCHY_REF != "master" ]]; then
   echo -e "\e[32mUsing branch: $OMARCHY_REF\e[0m"
   cd ~/.local/share/omarchy
